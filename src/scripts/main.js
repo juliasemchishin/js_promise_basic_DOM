0f5bb332c47673d68aa346fd3f18ccb3cb1fb667
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-11-13 16:18:21 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,40 +8,30 @@ const promise1 = new Promise((resolve, reject) => {
   const logo = document.querySelector('.logo');
 
   logo.addEventListener('click', () => {
-    const div = document.createElement('div');
-
-    div.classList.add('message');
-    div.textContent = 'Promise was resolved!';
-    body.appendChild(div);
     resolve('Promise was resolved!');
   });
 });
 
 const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const div = document.createElement('div');
-
-    div.classList.add('message');
-    div.classList.add('error-message');
-    div.textContent = 'Promise was rejected!';
-    body.appendChild(div);
     // eslint-disable-next-line prefer-promise-reject-errors
     reject('Promise was rejected!');
   }, 3000);
 });
 
-promise1
-  .then((successData) => {
-    return `'promise1 resolved', ${successData}`;
-  })
-  .catch((errorData) => {
-    return `'promise1 rejected', ${errorData}`;
-  });
+promise1.then((successData) => {
+  const div = document.createElement('div');
 
-promise2
-  .then((successData) => {
-    return `'promise2 resolved', ${successData}`;
-  })
-  .catch((errorData) => {
-    return `'promise2 rejected', ${errorData}`;
-  });
+  div.classList.add('message');
+  div.textContent = successData;
+  body.appendChild(div);
+});
+
+promise2.catch((errorData) => {
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.classList.add('error-message');
+  div.textContent = errorData;
+  body.appendChild(div);
+});
